6eea95e081ab30a81436ba0903baa5fd23015c99
louis <louishwh@gmail.com>
2018-03-19 20:58:41 +0800

    vim

--- PythonTools/TCP_client.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(('127.0.0.1',8000))
-
-
-dataStr = input("Say something:")
-s.send(dataStr.encode('utf-8'))
-
--- PythonTools/UDP_client.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-
-data_str = input("say something:  ")
-
-data = data_str.encode('utf-8')
-
-s.sendto(data,('127.0.0.1',8000))
